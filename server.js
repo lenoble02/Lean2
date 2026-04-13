ae478c5845d1b7ffdb5799e67f3253295378ca0f
TonNouveauPseudo <ton-nouvel-email@exemple.com>
2026-04-13 14:30:21 +0000

    Premier commit pour Lean2

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/')));
+
+// CONNEXION SÉCURISÉE (Utilise les variables de Render)
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+db.connect(err => {
+    if (err) console.error("❌ Erreur DB:", err.message);
+    else console.log("✅ Connecté à Aiven Cloud");
+});
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Serveur sur port ${PORT}`));
+
